864911fd052c95420df9e84e5651d375efdd7a18
CarMue <c.mueller@schule.bremen.de>
2021-09-30 10:58:57 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 // Import stylesheets
 import './style.css';
+import {Aktie} from './aktie';
+import {Schmuck} from './schmuck';
+import {Tresor} from './tresor';
 
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
@@ -9,3 +12,9 @@ let a1: Aktie = new Aktie(1, 97.88, 'BASF', 50.0);
 let a2: Aktie = new Aktie(2, 117.26, 'BAYER', 50.00);
 let s1: Schmuck = new Schmuck(3, 99.99, 'Brosche');
 let tresor: Tresor = new Tresor();
+
+tresor.addGegenstand(a1);
+tresor.addGegenstand(a2);
+tresor.addGegenstand(s1);
+
+console.log(tresor.toString());
